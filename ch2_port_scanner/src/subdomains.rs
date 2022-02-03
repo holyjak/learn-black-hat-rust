7f3e8785f5132ee485da73710b33c1ba53dfd020
Jakub Holy <jakubholy@jakubholy.net>
2022-02-03 20:46:31 +0100

    Set to rm duplicates, drop wildcard domains

--- ch2_port_scanner/src/subdomains.rs
+++ ch2_port_scanner/src/subdomains.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use crate::{ Error };
 
 use serde::{Deserialize};
@@ -10,22 +11,22 @@ pub struct CrtShResult {
     name_value: String
 }
 
-pub fn fetch_subdomains(domain: &str) -> Result<Vec<String>, Error> {
-    let crtResults = fetch_subdomains_int(domain)?;
-    Ok(res_to_subdomains(crtResults))
+pub fn fetch_subdomains(domain: &str) -> Result<HashSet<String>, Error> {
+    let crt_results = fetch_subdomains_int(domain)?;
+    Ok(res_to_subdomains(crt_results))
 }
 
-fn res_to_subdomains(results: Vec<CrtShResult>) -> Vec<String> {
+fn res_to_subdomains(results: Vec<CrtShResult>) -> HashSet<String> {
     results.iter()
     .map(|res| into_names(&res.name_value))
     .flatten()
-    // FIXME: rm duplicates
     .map(|s| s.to_string())
+    .filter(|s| !s.starts_with("*."))
     .collect()
 }
 
 fn into_names(name_value: &str) -> Vec<&str> {
-    name_value.split("\\n")
+    name_value.split("\n")
     .map(|s: &str| s.trim())
     .collect()
 }
